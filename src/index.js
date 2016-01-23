83bc7d39287e58e17a5f52cd3b704729e20dc124
Thomas Coopman <thomas.coopman@gmail.com>
2016-01-23 14:57:14 +0100

    fix ga import

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 'use strict';
 var ga = require('./ga');
 
-
-function analytics(state, options) {
-  if (!options) {
-    options = {};
-  }
-  options.page = state.path;
-  ga('send', 'pageview', options);
-}
-
-
-module.exports = analytics;
+module.exports = ga;
